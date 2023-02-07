dee473712236e7b434b9e662c2a3eb3fd59a402b
Linnzh <reg.lynnzh@gmail.com>
2023-02-07 12:08:42 +0800

    chore: 简化 Workman 配置

--- config/worker_server.php
+++ config/worker_server.php
@@ -4,43 +4,25 @@
 // | Workerman设置 仅对 php think worker:server 指令有效
 // +----------------------------------------------------------------------
 return [
-    // 扩展自身需要的配置
-    'protocol'       => 'websocket', // 协议 支持 tcp udp unix http websocket text
-    'host'           => '0.0.0.0', // 监听地址
-    'port'           => 2345, // 监听端口
-    'socket'         => '', // 完整监听地址
-    'context'        => [], // socket 上下文选项
-    'worker_class'   => '', // 自定义Workerman服务类名 支持数组定义多个服务
+    /**
+     * 以下信息可在 worker_class 服务类的类属性中配置
+     */
+    // 'protocol'       => 'websocket', // 协议 支持 tcp udp unix http websocket text
+    // 'host'           => '0.0.0.0', // 监听地址
+    // 'port'           => 2345, // 监听端口
+    // 'socket'         => '', // 完整监听地址
+    // 'context'        => [], // socket 上下文选项
 
-    // 支持workerman的所有配置参数
-    'name'           => 'thinkphp',
+    'worker_class'   => [
+        \app\admin\controller\WebsocketController::class
+    ], // 自定义Workerman服务类名 支持数组定义多个服务
+
+    /**
+     * 以下信息支持 workerman 的所有配置参数
+     * 可以在 worker_class 服务类的 $option 中配置
+     */
+    'name'           => 'worker',
     'count'          => 4,
     'daemonize'      => false,
     'pidFile'        => '',
-
-    // 支持事件回调
-    // onWorkerStart
-    'onWorkerStart'  => function ($worker) {
-
-    },
-    // onWorkerReload
-    'onWorkerReload' => function ($worker) {
-
-    },
-    // onConnect
-    'onConnect'      => function ($connection) {
-
-    },
-    // onMessage
-    'onMessage'      => function ($connection, $data) {
-        $connection->send('receive success');
-    },
-    // onClose
-    'onClose'        => function ($connection) {
-
-    },
-    // onError
-    'onError'        => function ($connection, $code, $msg) {
-        echo "error [ $code ] $msg\n";
-    },
 ];
